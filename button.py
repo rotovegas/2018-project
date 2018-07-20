9b26cd219b8c04887b78873e4f9fe524ed1d45f4
Gomah <stivinrotovegas@gmail.com>
2018-07-20 11:40:14 +0000

    Update button.py

--- button.py
+++ button.py
@@ -1,11 +1,15 @@
+#this is the codes for the Button
+#importing of libraries
 import paho.mqtt.client as mqtt
 from gpiozero import Button
 from signal import pause
 button = Button(4)
 
+#connection to network
 client = mqtt.Client()
 client.connect("broker.hivemq.com", 1883, 60)
 
+#adding the Location
 from geo import getLocation
 
 def button_press():
